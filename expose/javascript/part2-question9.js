a7c7dedd94e1a9373a8b129f25845b79a9d2f4ee
vivin2709 <97651150+vivin2709@users.noreply.github.com>
2023-04-30 23:42:35 -0700

    added
    cfdec

--- expose/javascript/part2-question9.js
+++ expose/javascript/part2-question9.js
@@ -1,18 +1,18 @@
 function discountPrices(prices, discount) {
     const discounted = [];
-    const finalPrice = 0;
+    const length = prices.length;
 
     for (let i = 0; i < prices.length; i++) {
         const discountedPrice = prices[i] * (1 - discount);
-        finalPrice = Math.round(discountedPrice * 100) / 100;
-        discounted.push(finalPrice);
+        discounted.push(discountedPrice);
     }
 
-    //  console.log(i);
+    //console.log(i);
+    //console.log(length);
     //  console.log(discountedPrice);
     //  console.log(finalPrice);
     //  console.log(discounted);
     //  console.log(length);
     return discounted;
 }
-discountPrices([100, 200, 300], 0.5);
+console.log(discountPrices([100, 200, 300], 0.5));
